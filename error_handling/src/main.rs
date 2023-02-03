763ccedde5647d08839b834923af168d1fc61db9
ahmet emrebas <noreply@authdare.com>
2023-02-03 16:49:19 -0600

    error handling

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,16 +1,43 @@
-use std::fs::File;
+use std::{
+    fs::File,
+    io::{self, ErrorKind, Read},
+};
 
 fn main() {
     // RUST_BACKTRACE=1 ./error_handling.exe
     // RUST_BACKTRACE=full ./error_handling.ex
     // panic!("Crash and burn");
 
-    let greeting_file_result = File::open("hello.txt");
+    let file_path = "files/hello.txt";
 
-    let result = match greeting_file_result {
-        Ok(_) => println!("File exist"),
-        Err(_) => println!("File Not exist"),
+    let greeting_file_result = File::open(&file_path);
+
+    let mut greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(&file_path) {
+                Ok(fc) => fc,
+                Err(e) => panic!("Could not create the file!"),
+            },
+            others => panic!("Could not open the file!, {:?}", others),
+        },
     };
 
-    println!("{:?}", greeting_file_result)
+    let mut file_content = String::from("");
+    greeting_file
+        .read_to_string(&mut file_content)
+        .expect("Could not read the file");
+
+    println!("File Content is {}", file_content);
+
+    let file_content = read_file_content(file_path).expect("Not read!");
+
+    println!("File content : {:?}", file_content);
+}
+
+fn read_file_content(file_path: &str) -> Result<String, io::Error> {
+    let mut file_content = String::new();
+
+    File::open(file_path)?.read_to_string(&mut file_content)?;
+    Ok(file_content)
 }
